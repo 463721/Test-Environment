c6a9fc4329edc0bb9a53cebe0b1910c6c183bc38
Ozan Oral <153126254+463721@users.noreply.github.com>
2024-11-06 10:09:43 -0500

    yuppp (#1)

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.95"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 1.12"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
+module "avm-res-resources-resourcegroup" {
+  source   = "Azure/avm-res-resources-resourcegroup/azurerm"
+  version  = "0.1.0"
+  location = "useast"
+  name     = "Testrg"
+}
